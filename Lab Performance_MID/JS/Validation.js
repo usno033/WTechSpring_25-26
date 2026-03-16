cac02c69ebb5ddb57e4c5c14b0196e9f48f95304
usno033 <usnokarmakar033@gmail.com>
2026-03-16 12:50:22 +0600

    lab performance

---
+++ Lab Performance_MID/JS/Validation.js
@@ -0,0 +1,26 @@
+const unitPrice = 1000;
+
+const quantityInput = document.getElementById("quantityperday");
+const totalPriceInput = document.getElementById("totalPrice");
+
+function calculateTotal(){
+
+let quantity = parseInt(quantityInput.value) || 0;
+
+if(quantity < 0){
+alert("Quantity cannot be negative. Resetting to 0.");
+quantity = 0;
+quantityInput.value = 0;
+}
+
+let total = unitPrice * quantity * 30;
+
+totalPriceInput.value = total;
+
+if(total > 1000){
+alert("Congratulations! they are eligible for a gift coupon.");
+}
+
+}
+
+quantityInput.addEventListener("input", calculateTotal);
